72f54a700f200e28ca78826388e6cfe755d9c0ee
MIGUEL PEDROSA DO CARMO NONATO <miguelpedrosacarmononato@gmail.com>
2023-11-16 10:25:16 -0300

    createTaskModel

--- src/main/java/com/miguelpnonatoo/todosimple/models/User.java
+++ src/main/java/com/miguelpnonatoo/todosimple/models/User.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -17,6 +18,8 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -49,9 +52,8 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 6, max = 60)
     private String password;
 
-    //private List<Task> tasks = new ArrayList<Task>();
-
-    
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
@@ -85,6 +87,16 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
 
     @Override
     public boolean equals(Object o) {
