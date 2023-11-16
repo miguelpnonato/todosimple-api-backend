bdfff348742c58439416dbae7817329c6fa01209
MIGUEL PEDROSA DO CARMO NONATO <miguelpedrosacarmononato@gmail.com>
2023-11-16 16:13:41 -0300

    createUserAndTaskRepositories

---
+++ src/main/java/com/miguelpnonatoo/todosimple/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package com.miguelpnonatoo.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.miguelpnonatoo.todosimple.models.User;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    
+
+
+    
+}
